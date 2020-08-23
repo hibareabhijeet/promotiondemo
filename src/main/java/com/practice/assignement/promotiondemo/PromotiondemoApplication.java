d3452024127262216602166525199e2d9f7f0bd5
Abhijit Hibare <hibareabhijeet@gmail.com>
2020-08-23 15:56:13 +0530

    new project creted for Promotion Engine Demo

---
+++ src/main/java/com/practice/assignement/promotiondemo/PromotiondemoApplication.java
@@ -0,0 +1,13 @@
+package com.practice.assignement.promotiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotiondemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/assignement/promotiondemo/PromotiondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.assignement.promotiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotiondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
